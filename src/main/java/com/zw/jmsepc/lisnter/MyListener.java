0c1ffd72a554c02a9e5dbe0e7e3a15b9d40ba797
zw <zw@192.168.1.111>
2017-04-19 10:03:47 +0800

    ..

---
+++ src/main/java/com/zw/jmsepc/lisnter/MyListener.java
@@ -0,0 +1,30 @@
+package com.zw.jmsepc.lisnter;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.zw.jmsepc.silkie.jms.DefaultJmsMessageListenerContainer;
+import com.zw.jmsepc.spring.SpringBeanManager;
+
+
+
+public class MyListener implements ServletContextListener {
+	DefaultJmsMessageListenerContainer traderResponseListenerContainer;
+	
+	private static Logger logger = Logger.getLogger(MyListener.class);
+	public void contextInitialized(ServletContextEvent evt) {
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:spring-context.xml");
+    	SpringBeanManager.initContext(ctx);
+		logger.info("ApplicationListener Starting...............................");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		
+	}
+	
+}
